c1f668a2fc68c87e31fb23fad9d0bf79cecceee7
teosoares10 <teosoares10@gmail.com>
2023-08-27 18:06:02 +0100

    style(header): remove relative position from tag header

--- src/components/Layouts/Header/index.tsx
+++ src/components/Layouts/Header/index.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center p-4 relative">
+    <header className="flex justify-between items-center p-4">
       <div className="text-center">
         <h2 className="font-bold text-xl capitalize">krazy kitchen katerers</h2>
         <p className="text-xs tracking-[.4rem] uppercase">restaurant</p>
