fb112750b32d530cd2e9235ab90259b7d038d290
teosoares10 <teosoares10@gmail.com>
2023-08-27 14:49:36 +0100

    feat(image): add image component

---
+++ src/components/Image/index.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+type ImgProps = {
+  src: string;
+  width: number;
+  height: number;
+  alt?: string;
+};
+
+export default function Img({ src, width, height, alt }: ImgProps) {
+  return <Image src={src} width={width} height={height} alt={`${alt}`} />;
+}
