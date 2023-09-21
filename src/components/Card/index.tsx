a56f96a61657ce0c424e77c92dc8fe859252e82d
teosoares10 <teosoares10@gmail.com>
2023-09-21 12:57:41 +0100

    refactor(card): add differents card's types

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,14 +1,57 @@
+import Link from 'next/link';
+import Img from '../Image';
+import SocialMedia from '../SocialMedia';
+import { Button } from '../ui/button';
+
 type CardProps = {
+  type: 'menu' | 'service' | 'chef';
+  src?: string;
+  alt?: string;
   title: string;
   description: string;
+  price?: string;
 };
 
-export default function Card({ title, description }: CardProps) {
+export default function Card({
+  type,
+  src,
+  alt,
+  title,
+  description,
+  price
+}: CardProps) {
   return (
-    <section className="space-y-4 text-center bg-blue-600 px-20 py-28 shadow-md hover:shadow-lg hover:shadow-gray-700 shadow-gray-700 transition-shadow duration-500">
-      <div>Image / icon</div>
-      <h2 className="font-bold text-2xl capitalize">{title}</h2>
-      <p>{description}</p>
+    <section className="w-[20rem] h-[20rem] space-y-4 bg-blue-600 px-10 py-28 shadow-md hover:shadow-lg hover:shadow-gray-700 shadow-gray-700 transition-shadow duration-500">
+      {type === 'service' ? (
+        <>
+          <div>Icon</div>
+          <h2 className="font-bold text-2xl capitalize">{title}</h2>
+          <p>{description}</p>
+        </>
+      ) : null}
+      {type === 'menu' ? (
+        <>
+          <div className="relative">
+            <Img src={`${src}`} alt={alt} />
+            <span className="absolute top-0 right-0">{price}</span>
+          </div>
+          <h2 className="font-bold text-2xl capitalize">{title}</h2>
+          <p>{description}</p>
+          <Button>
+            <Link href={''}>Order Now!</Link>
+          </Button>
+        </>
+      ) : null}
+      {type === 'chef' ? (
+        <>
+          <div className="relative">
+            <Img src={`${src}`} alt={alt} />
+          </div>
+          <h2 className="font-bold text-2xl capitalize">{title}</h2>
+          <p>{description}</p>
+          <SocialMedia />
+        </>
+      ) : null}
     </section>
   );
 }
