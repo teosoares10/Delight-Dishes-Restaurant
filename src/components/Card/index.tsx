d7a624a1667303abea76882ffc725801d481f620
teosoares10 <teosoares10@gmail.com>
2023-09-21 13:50:05 +0100

    style(card): remove width, height and set padding to 8

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,12 +1,10 @@
 import Link from 'next/link';
-import Img from '../Image';
 import SocialMedia from '../SocialMedia';
 import { Button } from '../ui/button';
 
 type CardProps = {
   type: 'menu' | 'service' | 'chef';
-  src?: string;
-  alt?: string;
+  img?: React.ReactNode;
   title: string;
   description: string;
   price?: string;
@@ -14,14 +12,13 @@ type CardProps = {
 
 export default function Card({
   type,
-  src,
-  alt,
+  img,
   title,
   description,
   price
 }: CardProps) {
   return (
-    <section className="w-[20rem] h-[20rem] space-y-4 bg-blue-600 px-10 py-28 shadow-md hover:shadow-lg hover:shadow-gray-700 shadow-gray-700 transition-shadow duration-500">
+    <section className="space-y-4 bg-blue-600 px-10 py-8 shadow-md hover:shadow-lg hover:shadow-gray-700 shadow-gray-700 transition-shadow duration-500">
       {type === 'service' ? (
         <>
           <div>Icon</div>
@@ -32,7 +29,7 @@ export default function Card({
       {type === 'menu' ? (
         <>
           <div className="relative">
-            <Img src={`${src}`} alt={alt} />
+            {img}
             <span className="absolute top-0 right-0">{price}</span>
           </div>
           <h2 className="font-bold text-2xl capitalize">{title}</h2>
@@ -44,9 +41,7 @@ export default function Card({
       ) : null}
       {type === 'chef' ? (
         <>
-          <div className="relative">
-            <Img src={`${src}`} alt={alt} />
-          </div>
+          <div className="relative">{img}</div>
           <h2 className="font-bold text-2xl capitalize">{title}</h2>
           <p>{description}</p>
           <SocialMedia />
