7acba1e2b90307a3f0ecdbb2e28fa67e0b5d1f86
teosoares10 <teosoares10@gmail.com>
2023-09-30 13:42:50 +0100

    refactor(icons): change import path for Coffee Food and Meal components

--- src/components/Icons/RestaurantServices/index.tsx
+++ src/components/Icons/RestaurantServices/index.tsx
@@ -1,6 +1,6 @@
-import Coffee from './Coffee';
-import Food from './Food';
-import Meal from './Meal';
+import Coffee from './components/Coffee';
+import Food from './components/Food';
+import Meal from './components/Meal';
 
 export default function IconService({ icon }: { icon: string }) {
   return (
