45d4776ff132fe23ee5c4ef58fe2388633028963
teosoares10 <teosoares10@gmail.com>
2023-09-30 13:36:26 +0100

    refactor(icons): create link list

--- src/components/Icons/SocialMedia/index.tsx
+++ src/components/Icons/SocialMedia/index.tsx
@@ -1,20 +1,37 @@
 import Link from 'next/link';
-import FacebookLogo from '@/components/Icons/SocialMedia/Facebook';
-import TwitterLogo from '@/components/Icons/SocialMedia/Twitter';
-import InstagramLogo from '@/components/Icons/SocialMedia/Instagram';
+import FacebookLogo from '@/components/Icons/SocialMedia/components/Facebook';
+import TwitterLogo from '@/components/Icons/SocialMedia/components/Twitter';
+import InstagramLogo from '@/components/Icons/SocialMedia/components/Instagram';
+import { Button } from '@/components/ui/button';
 
 export default function SocialMedia() {
+  const Links = [
+    {
+      id: 1,
+      link: 'https://instagram.com',
+      logo: <InstagramLogo />
+    },
+    {
+      id: 2,
+      link: 'https://facebook.com',
+      logo: <FacebookLogo />
+    },
+    {
+      id: 3,
+      link: 'https://twitter.com',
+      logo: <TwitterLogo />
+    }
+  ];
+
   return (
     <div className="flex gap-4">
-      <Link href="https://instagram.com" target="_blank">
-        <InstagramLogo />
-      </Link>
-      <Link href="https://facebook.com" target="_blank">
-        <FacebookLogo />
-      </Link>
-      <Link href="https://twitter.com" target="_blank">
-        <TwitterLogo />
-      </Link>
+      {Links.map(({ id, link, logo }) => (
+        <Button className="px-0" key={id} asChild>
+          <Link className="hover:text-orange-600" href={link} target="_blank">
+            {logo}
+          </Link>
+        </Button>
+      ))}
     </div>
   );
 }
