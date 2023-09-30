f1b04e6acb5f46e45a957d56a928c57bb353e4eb
teosoares10 <teosoares10@gmail.com>
2023-09-30 13:29:31 +0100

    refactor(icons): create SocialMedia components folder
    add Facebook Instagram and Twitter components to SocialMedia components folder

---
+++ src/components/Icons/SocialMedia/components/Facebook.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { FacebookLogo as Facebook } from '@phosphor-icons/react';
+
+export default function FacebookLogo() {
+  return <Facebook size={24} />;
+}
---
+++ src/components/Icons/SocialMedia/components/Instagram.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { InstagramLogo as Instagram } from '@phosphor-icons/react';
+
+export default function InstagramLogo() {
+  return <Instagram size={24} />;
+}
