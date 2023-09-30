49848c1f22f4ab316b31830cdcff2aa21e681092
teosoares10 <teosoares10@gmail.com>
2023-09-30 09:53:11 +0100

    refactor(image): add tailwind cn function

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,16 +1,24 @@
+import { cn } from '@/lib/utils';
 import Image from 'next/image';
 
 type ImgProps = {
+  className?: string;
   src: string;
   width?: number;
   height?: number;
   alt?: string;
 };
 
-export default function Img({ src, width = 300, height = 500, alt }: ImgProps) {
+export default function Img({
+  className,
+  src,
+  alt,
+  width = 200,
+  height = 500
+}: ImgProps) {
   return (
     <Image
-      className="bg-slate-600"
+      className={cn('bg-slate-600', className)}
       src={src}
       width={width}
       height={height}
