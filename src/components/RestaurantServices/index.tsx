a92e29582ff29d5e7588f081e5b5e105b58a069f
teosoares10 <teosoares10@gmail.com>
2023-09-30 09:57:08 +0100

    refactor(restaurantservices): add ServicesItem

--- src/components/RestaurantServices/index.tsx
+++ src/components/RestaurantServices/index.tsx
@@ -1,14 +1,13 @@
 import { services } from '@/db/Local/services';
-import Card from '../Card';
 import IconService from '../Icons/RestaurantServices';
+import ServicesItem from './components/ServicesItem';
 
 export default function RestaurantServices() {
   return (
     <div className="flex justify-center gap-8 text-center mt-24">
       {services.map((service) => (
-        <Card
+        <ServicesItem
           key={service.id}
-          type="service"
           icon={<IconService icon={service.type} />}
           title={service.title}
           description={service.description}
