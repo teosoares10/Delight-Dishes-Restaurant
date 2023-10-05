f55bd7aa239e8958cd61ef27ad8e8c54da35403a
teosoares10 <teosoares10@gmail.com>
2023-10-05 11:43:56 +0100

    refactor(nav): remove navigation-menu component and create a custom menu navigation

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -2,102 +2,32 @@
 
 import Link from 'next/link';
 
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle
-} from '@/components/ui/navigation-menu';
-
-import MenuItem from '@/components/List/MenuItem';
-
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: 'Our Chefs',
-    href: '/ourChefs',
-    description: 'Figure it out who are our kitchen chefs.'
-  },
-  {
-    title: 'Recipe',
-    href: '/recipe',
-    description:
-      'Learn more about our recipe and try it at home, but come and tasty it before you try it at home.'
-  },
-  {
-    title: 'Gallery',
-    href: '/gallery',
-    description:
-      'Checl out what did and what we are doing enjoy it through your memories.'
-  },
-  {
-    title: 'FQA',
-    href: '/faq',
-    description:
-      'See what are the frequent questions of our visitor and see their answers.'
-  }
-];
-
 export default function NavBar() {
   return (
     <>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href="/" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Home
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/aboutUs" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                About Us
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/ourMenu" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Our Menu
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>More</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid w-[40rem] gap-3 p-4 md:grid-cols-2">
-                {components.map((component) => (
-                  <MenuItem
-                    key={component.title}
-                    title={component.title}
-                    href={component.href}
-                  >
-                    {component.description}
-                  </MenuItem>
-                ))}
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/blog" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Blog
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/contactUs" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Contact Us
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+      <ul className="flex gap-4">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/aboutUs">About Us</Link>
+        </li>
+        <li>
+          <Link href="/ourMenu">Our Menu</Link>
+        </li>
+        <li>
+          <Link href="/gallery">Gallery</Link>
+        </li>
+        <li>
+          <Link href="/recipe">Recipe</Link>
+        </li>
+        <li>
+          <Link href="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
     </>
   );
 }
