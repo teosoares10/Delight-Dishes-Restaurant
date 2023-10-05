101b8d8ccb450d18b94525b0fa7ccc292de57cd1
teosoares10 <teosoares10@gmail.com>
2023-10-05 11:41:01 +0100

    feat(home): add posts component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Chef from '@/components/Chefs';
 import RestaurantServices from '@/components/RestaurantServices';
 import OurStory from '@/components/OurStory';
 import Banner from '@/components/Banner';
+import Posts from '@/components/Posts';
 
 export default function Home() {
   return (
@@ -16,6 +17,8 @@ export default function Home() {
       <Dishes />
 
       <Chef />
+
+      <Posts />
     </main>
   );
 }
