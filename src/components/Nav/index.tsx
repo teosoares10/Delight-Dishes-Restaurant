33120e92a9ed9e5ce813f49663dade636f264f16
teosoares10 <teosoares10@gmail.com>
2023-10-11 13:46:13 +0100

    refactor(nav): add cn lib and change style

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,31 +1,46 @@
 'use client';
 
+import { cn } from '@/lib/utils';
 import Link from 'next/link';
 
-export default function NavBar() {
+export default function NavBar({ className }: { className?: string }) {
   return (
     <>
-      <ul className="flex gap-4">
+      <ul className={cn('flex gap-4', className)}>
         <li>
-          <Link href="/">Home</Link>
+          <Link className="hover:text-orange-600" href="/">
+            Home
+          </Link>
         </li>
         <li>
-          <Link href="/aboutUs">About Us</Link>
+          <Link className="hover:text-orange-600" href="/aboutUs">
+            About Us
+          </Link>
         </li>
         <li>
-          <Link href="/ourMenu">Our Menu</Link>
+          <Link className="hover:text-orange-600" href="/ourMenu">
+            Our Menu
+          </Link>
         </li>
         <li>
-          <Link href="/gallery">Gallery</Link>
+          <Link className="hover:text-orange-600" href="/gallery">
+            Gallery
+          </Link>
         </li>
         <li>
-          <Link href="/recipe">Recipe</Link>
+          <Link className="hover:text-orange-600" href="/recipe">
+            Recipe
+          </Link>
         </li>
         <li>
-          <Link href="/blog">Blog</Link>
+          <Link className="hover:text-orange-600" href="/blog">
+            Blog
+          </Link>
         </li>
         <li>
-          <Link href="/contact">Contact</Link>
+          <Link className="hover:text-orange-600" href="/contact">
+            Contact
+          </Link>
         </li>
       </ul>
     </>
