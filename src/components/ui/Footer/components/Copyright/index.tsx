bb5586f22d0638d5de2b1dd6395326da36abe83d
teosoares10 <teosoares10@gmail.com>
2023-12-03 00:05:16 +0100

    feat(copyright): create footer component called copyright

---
+++ src/components/ui/Footer/components/Copyright/index.tsx
@@ -0,0 +1,8 @@
+export default function Copyright() {
+  return (
+    <div className="flex justify-between py-4 px-12" data-testid="copyright">
+      <span data-testid="span">Copyright &copy; 2023 All rights reserved</span>
+      <span data-testid="span">Made with (love) by Teodoro Pedro</span>
+    </div>
+  );
+}
