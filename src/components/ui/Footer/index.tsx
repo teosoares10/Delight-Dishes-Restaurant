03df431b338dc7eb918ff8fafd5bc415238b94d2
teosoares10 <teosoares10@gmail.com>
2023-12-02 23:30:41 +0100

    feat(footer): create footer component

---
+++ src/components/ui/Footer/index.tsx
@@ -0,0 +1,27 @@
+import ContactUs from './components/ContactUs';
+import OpenTime from './components/OpenTime';
+import Instagram from './components/Instagram';
+import About from './components/About';
+import Menu from './components/Menu';
+import Copyright from './components/Copyright';
+import Newsletter from './components/Newsletter';
+
+export default function Footer() {
+  return (
+    <footer className="mt-24 pt-24 space-y-8" data-testid="footer">
+      <Newsletter />
+
+      <div className="flex justify-between px-12">
+        <About />
+        <Menu />
+        <Instagram />
+        <div className="space-y-8">
+          <ContactUs />
+          <OpenTime />
+        </div>
+      </div>
+
+      <Copyright />
+    </footer>
+  );
+}
