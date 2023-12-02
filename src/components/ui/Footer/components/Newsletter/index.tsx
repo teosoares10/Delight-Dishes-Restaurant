ace4217ac6b3c5ecba542c15add36106b8ce6939
teosoares10 <teosoares10@gmail.com>
2023-12-03 00:21:44 +0100

    feat(footer): create footer component called newsletter

---
+++ src/components/ui/Footer/components/Newsletter/index.tsx
@@ -0,0 +1,27 @@
+import Title from '@/components/ui/Typography/Title';
+import { Button } from '@/components/ui/button';
+
+export default function Newsletter() {
+  return (
+    <div className="flex h-[10rem] bg-gray-400" data-testid="newsletter">
+      <div className="w-[30rem] bg-gray-500 pl-12 pt-14">
+        <span className="uppercase" data-testid="span">
+          subscribe to our
+        </span>
+        <Title className="text-2xl text-left uppercase">newsletter</Title>
+      </div>
+      <div className="flex-1 pl-48 pt-14">
+        <div className="flex items-center gap-1 bg-white p-2 w-[40rem] rounded-full">
+          <input
+            className="w-full h-9 text-black pl-2 rounded-full"
+            type="text"
+            placeholder="Enter email address"
+          />
+          <Button className="w-[10rem] bg-orange-600 hover:bg-orange-400 rounded-full uppercase">
+            submit
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
