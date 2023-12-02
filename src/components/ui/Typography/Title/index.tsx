be806513055a83a4f4bbc79a7b937d64a3c2b91f
teosoares10 <teosoares10@gmail.com>
2023-12-02 23:24:28 +0100

    feat(title.tsx): create title component

---
+++ src/components/ui/Typography/Title/index.tsx
@@ -0,0 +1,20 @@
+import { cn } from '@/lib/utils';
+
+type TitleProps = {
+  children: string;
+  className?: string;
+};
+
+export default function Title({ children, className }: TitleProps) {
+  return (
+    <h1
+      className={cn(
+        ' text-center text-gray-700 font-bold capitalize',
+        className
+      )}
+      data-testid="title"
+    >
+      {children}
+    </h1>
+  );
+}
