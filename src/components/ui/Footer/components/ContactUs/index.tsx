5e257b751a7a9ffb87b19519296b794553e52a5c
teosoares10 <teosoares10@gmail.com>
2023-12-04 00:00:18 +0100

    refactor: add contact with icon component on contact us component

--- src/components/ui/Footer/components/ContactUs/index.tsx
+++ src/components/ui/Footer/components/ContactUs/index.tsx
@@ -1,29 +1,24 @@
 import Title from '@/components/ui/Typography/Title';
-import PhoneIcon from '@/components/ui/Icons/Phone';
-import MapPinIcon from './components/Icon/MapPin';
-import MailIcon from '@/components/ui/Icons/Mail';
-
-// TODO: create ContactWithIcon component
+import { EnvelopeSimple, MapPin, Phone } from '@phosphor-icons/react';
+import ContactWithIcon from './components/ContactWithIcon';
 
 export default function ContactUs() {
   return (
     <div className="space-y-4" data-testid="contact-us">
       <Title className="text-xl text-left">contact us</Title>
       <div className="space-y-4">
-        <div className="flex gap-4 items-center">
-          <MapPinIcon />
-          <span className="block" data-testid="span">
-            address
-          </span>
-        </div>
-        <div className="flex gap-4 items-center">
-          <PhoneIcon />
-          <span data-testid="span">(+244) 941477502</span>
-        </div>
-        <div className="flex gap-4 items-center">
-          <MailIcon />
-          <span data-testid="span">teosoares10@gmail.com</span>
-        </div>
+        <ContactWithIcon
+          icon={<MapPin size={20} data-testid="map-pin-icon" />}
+          contact="address"
+        />
+        <ContactWithIcon
+          icon={<EnvelopeSimple size={20} data-testid="mail-icon" />}
+          contact="teosoares10@gmail.com"
+        />
+        <ContactWithIcon
+          icon={<Phone size={20} data-testid="phone-icon" />}
+          contact="(+244) 941477502"
+        />
       </div>
     </div>
   );
