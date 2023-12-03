19a1b1b757b8882c858b23a5ccf8bc7e85a7f10b
teosoares10 <teosoares10@gmail.com>
2023-12-03 19:54:48 +0100

    feat(src): create pages router folder

---
+++ src/pages/index.tsx
@@ -0,0 +1,47 @@
+import Posts from '@/components/Post';
+import RestaurantServices from '@/components/RestaurantService';
+import Banner from '@/components/ui/Banner';
+import BookTable from '@/components/ui/BookTable';
+import Chef from '@/components/ui/Chef';
+import Dishes from '@/components/ui/Dish';
+import OurStory from '@/components/ui/OurStory';
+import Head from 'next/head';
+import { ChefApiResponse, ChefAttributes } from 'type';
+
+export default function Home({ attributes }: ChefApiResponse) {
+  return (
+    <div>
+      <Head>
+        <title>Teodoro Eliesel Pedro - Restaurant</title>
+      </Head>
+      <main>
+        <Banner />
+        <OurStory />
+        <RestaurantServices />
+        <Dishes />
+        <Chef attributes={attributes} />
+        <BookTable />
+        <Posts />
+      </main>
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/chefs?populate=*`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_TOKEN}`
+      }
+    }
+  );
+
+  const chefs = await res.json();
+  const attributes: ChefAttributes[] = chefs.data.map(
+    ({ attributes }: ChefApiResponse) => attributes
+  );
+  return {
+    props: { attributes }
+  };
+}
