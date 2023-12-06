c9bee5effa36bfd6d0d0def4b6701f9a96d4ca0a
teosoares10 <teosoares10@gmail.com>
2023-12-06 14:54:05 +0100

    feat: create a form schema with zod

---
+++ src/validation/FormSchema.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod';
+
+const formSchema = z.object({
+  fullName: z.string().min(3),
+  email: z.string().email(),
+  phone: z.string(),
+  numbersOfGuests: z.number().int().positive().max(50),
+  date: z.coerce
+    .date()
+    .refine((date) => date >= new Date(), { message: 'Data inválida' }),
+  time: z.string().refine(
+    (time) => {
+      const timeNow = new Date().getHours();
+
+      if (timeNow <= Number.parseInt(time)) return true;
+    },
+    {
+      message: 'Hora invalida'
+    }
+  ),
+  specialRequest: z.string(),
+  allergiesOrDietaryRestrictions: z.string(),
+  promoCode: z.string(),
+  agreement: z.boolean()
+});
+
+export default formSchema;
