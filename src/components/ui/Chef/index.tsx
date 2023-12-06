d0eb5976c327610e33b4b2191c308bd4c39af409
teosoares10 <teosoares10@gmail.com>
2023-12-06 15:19:33 +0100

    refactor(chef): add title, social media and image component to chef component

--- src/components/ui/Chef/index.tsx
+++ src/components/ui/Chef/index.tsx
@@ -6,27 +6,29 @@ import SocialMedia from '@/components/ui/Icons/SocialMedia';
 export default function Chef({ attributes }: ChefApiResponse) {
   return (
     <div className="space-y-16 py-36 px-12">
-      <h1
-        style={cormorantInfant.style}
-        className="text-4xl font-bold capitalize"
-      >
-        our masters chefs
-      </h1>
-      <div className="h-[20rem] flex justify-between">
-        {chefs.map((chef) => (
-          <ChefItem
-            key={chef.id}
-            img={
-              <Img
-                className="w-full h-full"
-                src={chef.src}
-                width={200}
-                height={250}
-              />
-            }
-            name={chef.name}
-            role={chef.role}
-          />
+      <Title className="text-4xl">our masters chefs</Title>
+      <div className="h-[20rem] flex gap-4">
+        {attributes?.map((chef: ChefAttributes, index: number) => (
+          <div
+            key={index}
+            className="w-[20rem] space-y-4"
+            data-testid="chef-item"
+          >
+            <Img
+              className="w-full h-full"
+              src={`${chef.img}`}
+              alt={`${chef.alt}`}
+              width={200}
+              height={250}
+            />
+            <Title className="text-2xl text-left">{chef.name}</Title>
+            <span className="block capitalize">{chef.role}</span>
+            <SocialMedia
+              facebook={chef.facebook}
+              instagram={chef.instagram}
+              twitter={chef.twitter}
+            />
+          </div>
         ))}
       </div>
     </div>
