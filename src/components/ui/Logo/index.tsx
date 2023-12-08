edaa17936d20b8228067d74ec964db5c93e67a39
teosoares10 <teosoares10@gmail.com>
2023-12-08 02:43:19 +0100

    feat: create logo component

---
+++ src/components/ui/Logo/index.tsx
@@ -0,0 +1,11 @@
+import Text from '../Typography/Text';
+import Title from '../Typography/Title';
+
+export default function Logo() {
+  return (
+    <div className="text-center" data-testid="logo">
+      <Title className="text-xl tracking-[.2rem]">delight dishes</Title>
+      <Text className="text-xs tracking-[.3rem] uppercase">restaurant</Text>
+    </div>
+  );
+}
