930e4dee11a268d80390be993c6e024381906d52
teosoares10 <teosoares10@gmail.com>
2023-12-10 12:34:57 +0100

    feat(form): create input component

---
+++ src/components/ui/Form/components/Input/index.tsx
@@ -0,0 +1,26 @@
+import { cn } from '@/lib/utils';
+import { forwardRef, InputHTMLAttributes, Ref } from 'react';
+
+type InputProps = InputHTMLAttributes<HTMLInputElement>;
+
+const Input = forwardRef(
+  (
+    { type = 'text', className, ...props }: InputProps,
+    ref: Ref<HTMLInputElement>
+  ) => (
+    <input
+      ref={ref}
+      className={
+        (cn('bg-background border-white border-b placeholder:text-white'),
+        className)
+      }
+      type={type}
+      data-testid="input"
+      {...props}
+    />
+  )
+);
+
+Input.displayName = 'Input';
+
+export default Input;
