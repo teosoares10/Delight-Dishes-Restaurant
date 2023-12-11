18005b0e9cc0d90a6dc9e97788f75aa9882d8ea5
teosoares10 <teosoares10@gmail.com>
2023-12-11 12:05:49 +0100

    feat(header): create header component
    add logo, navbar, button components

---
+++ src/components/ui/Header/index.tsx
@@ -0,0 +1,27 @@
+import Logo from '@/components/ui/Logo';
+import NavBar from '@/components/ui/Nav';
+import { Button } from '@/components/ui/button';
+
+export default function Header() {
+  return (
+    <header
+      className="flex justify-between items-center p-4"
+      data-testid="header"
+    >
+      <Logo />
+      <NavBar />
+      <Button
+        className="text-white hover:text-orange-500 border border-orange-500 bg-orange-500"
+        asChild
+      >
+        <a
+          href="#bookTable"
+          className="block capitalize"
+          data-testid="book-table-link"
+        >
+          book a table
+        </a>
+      </Button>
+    </header>
+  );
+}
