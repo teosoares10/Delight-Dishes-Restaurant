f2312c383680076e7ad9c2a065288632bbd43389
teosoares10 <teosoares10@gmail.com>
2023-12-11 23:36:04 +0100

    refactor(restaurantservice): change icons components size

--- src/components/ui/RestaurantService/components/Icons/Coffee.tsx
+++ src/components/ui/RestaurantService/components/Icons/Coffee.tsx
@@ -1,5 +1,5 @@
 import { Coffee } from '@phosphor-icons/react';
 
 export default function CoffeeIcon() {
-  return <Coffee size={76} data-testid="icon-coffee" />;
+  return <Coffee size={55} data-testid="icon-coffee" />;
 }
