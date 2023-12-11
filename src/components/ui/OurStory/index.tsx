35b7cc7cd612a8375b9165d087abd49f35040836
teosoares10 <teosoares10@gmail.com>
2023-12-11 12:41:50 +0100

    refactor(ourstory): make some modification in our story component
    remove cormorantInfant font
     add text, title, link and button components
     add data-testid  at our
    story component
     change px-8 to px-12
     add src in img components

--- src/components/ui/OurStory/index.tsx
+++ src/components/ui/OurStory/index.tsx
@@ -1,27 +1,41 @@
-import { cormorantInfant } from '../../../public/fonts';
+import Link from 'next/link';
 import Img from '../Image';
-import { Button } from '../ui/button';
+import Text from '@/components/ui/Typography/Text';
+import Title from '@/components/ui/Typography/Title';
+import { Button } from '@/components/ui/button';
 
 export default function OurStory() {
   return (
-    <div className="flex justify-between items-center py-36 px-8">
+    <div
+      className="flex justify-between items-center py-36 px-12"
+      data-testid="our-story"
+    >
       <div className="space-y-8">
-        <h1
-          style={cormorantInfant.style}
-          className="text-4xl font-bold capitalize"
-        >
-          our story
-        </h1>
-        <p className="w-[35rem]">
+        <Title className="text-4xl text-left">our story</Title>
+        <Text className="w-[35rem]">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit accusamus
           doloremque nesciunt eos earum cumque est...
-        </p>
-        <Button className="capitalize bg-blue-500">learn more</Button>
+        </Text>
+        <Button className="capitalize bg-orange-600" asChild>
+          <Link href={``} data-testid="link">
+            learn more
+          </Link>
+        </Button>
       </div>
 
       <div className="flex gap-4">
-        <Img src="" width={350} height={450} alt="" />
-        <Img src="" width={350} height={450} alt="" />
+        <Img
+          src="https://images.unsplash.com/photo-1583394293214-28ded15ee548?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80"
+          width={350}
+          height={450}
+          alt=""
+        />
+        <Img
+          src="https://images.unsplash.com/photo-1583394293214-28ded15ee548?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80"
+          width={350}
+          height={450}
+          alt=""
+        />
       </div>
     </div>
   );
