ae61be645ee744e25e7bd3d8da341018653f5915
teosoares10 <teosoares10@gmail.com>
2023-12-11 12:01:19 +0100

    refactor(dish): make some modifications in dish item component
    add title and button component
     change styles:
     change bg-black to bg-white/90 at dish component
    
    add text-white bg-gray-600 at span
     add text-base
      add text-white hover:bg-background
    hover:border hover:border-orange hover:text-orange-500

--- src/components/ui/Dish/components/List/DishItem.tsx
+++ src/components/ui/Dish/components/List/DishItem.tsx
@@ -1,6 +1,7 @@
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
-import { Button } from '../../../ui/button';
+import { Button } from '@/components/ui/button';
+import Title from '@/components/ui/Typography/Title';
 
 type DishItemProps = {
   className?: string;
@@ -18,28 +19,30 @@ export default function DishItem({
   return (
     <div
       className={cn(
-        'flex flex-col gap-8 justify-center items-center w-[20rem] h-[28rem] bg-black px-4 py-14',
+        'flex flex-col gap-8 justify-center items-center w-[20rem] h-[28rem] bg-white/90 px-4 py-14',
         className
       )}
     >
       <div className="flex flex-col justify-center items-center gap-8">
         <div className="w-[10rem] h-[10rem] relative text-center">
           {img}
-          <span className="flex justify-center items-center w-10 h-10 p-4 bg-green-600 absolute bottom-4 right-2 rounded-full">
+          <span className="flex justify-center items-center w-10 h-10 p-4 text-white bg-gray-600 absolute bottom-4 right-2 rounded-full">
             ${price}
           </span>
         </div>
         <Button asChild>
           <Link href={''}>
-            <h2 className="font-bold capitalize hover:text-orange-600">
-              {title}
-            </h2>
+            <Title className="text-base hover:text-orange-600">{title}</Title>
           </Link>
         </Button>
       </div>
 
       <div className="mt-auto">
-        <Button className="bg-orange-600 block" variant={'link'} asChild>
+        <Button
+          className="bg-orange-600 block text-white hover:bg-background hover:border hover:border-orange-500 hover:text-orange-500"
+          variant={'link'}
+          asChild
+        >
           <Link href={''}>Order Now!</Link>
         </Button>
       </div>
