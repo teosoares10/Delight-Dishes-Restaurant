d7dd1efc512ca398b8a6d654a2e224f5a6091886
teosoares10 <teosoares10@gmail.com>
2023-12-11 17:19:38 +0100

    style(hero): set width to 40rem and add height

--- src/components/ui/Hero/index.tsx
+++ src/components/ui/Hero/index.tsx
@@ -24,7 +24,7 @@ export default function Hero() {
           view menu
         </Button>
       </div>
-      <div className="w-[30rem] bg-gray-500">
+      <div className="w-[40rem] h-[30rem] bg-gray-500">
         <Img
           className="w-full h-full"
           src="https://images.unsplash.com/photo-1484659619207-9165d119dafe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
