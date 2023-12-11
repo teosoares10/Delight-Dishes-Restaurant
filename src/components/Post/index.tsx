3fd38d804f496d485dcc269e0c42976a92afc2e8
teosoares10 <teosoares10@gmail.com>
2023-12-11 13:03:33 +0100

    refactor(post): make some modifications in post component
    remove post item component
     change img component module path
     add link component
     add title and
    text components
     add data-testid at post component
     add li with key and data-testid

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,30 +1,36 @@
 import { posts } from '@/constants/Local/posts';
-import PostItem from './components/PostItem';
-import Img from '../Image';
+import Link from 'next/link';
+import Img from '@/components/ui/Image';
+import Title from '../ui/Typography/Title';
+import Text from '../ui/Typography/Text';
 
 export default function Posts() {
   return (
-    <div className="mt-24 px-12 pt-24 space-y-4">
+    <div className="mt-24 px-12 pt-24 space-y-16" data-testid="posts">
       <div>
-        <h2>Our Posts</h2>
+        <Title className="text-4xl">Our Posts</Title>
       </div>
       <ul className="flex justify-between">
         {posts.map((post) => (
-          <li key={post.id}>
-            <PostItem
-              className="w-[18rem] h-[15rem]"
-              img={
-                <Img
-                  className="w-full h-full"
-                  src={post.img}
-                  alt=""
-                  width={300}
-                  height={250}
-                />
-              }
-              date={post.date}
-              title={post.title}
+          <li key={post.id} data-testid="post-item">
+            <Img
+              className="w-full h-full"
+              src={post.img}
+              alt=""
+              width={300}
+              height={250}
             />
+            <div className="space-y-4">
+              <span data-testid="date">{post.date}</span>
+              <Link
+                href=""
+                className="block font-bold hover:text-orange-600"
+                data-testid="link"
+              >
+                {post.title}
+              </Link>
+              {post.description ? <Text>{post.description}</Text> : null}
+            </div>
           </li>
         ))}
       </ul>
