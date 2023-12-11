ac9cb4adef13bcffe34e68c82b071736a4d7b866
teosoares10 <teosoares10@gmail.com>
2023-12-11 12:04:02 +0100

    feat(booktable): create book table component
    add title and form components

---
+++ src/components/ui/BookTable/index.tsx
@@ -0,0 +1,14 @@
+import Form from '../Form';
+import Title from '../Typography/Title';
+
+export default function BookTable() {
+  return (
+    <div
+      className="flex flex-col gap-8 justify-center items-center bg-gray-300 mt-32 py-32"
+      data-testid="book-table"
+    >
+      <Title className="text-3xl">book a table</Title>
+      <Form />
+    </div>
+  );
+}
