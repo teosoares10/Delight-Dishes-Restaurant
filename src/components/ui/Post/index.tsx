a006fd8db56a5b9e3316923b0817024d1af7616b
teosoares10 <teosoares10@gmail.com>
2023-12-12 17:45:14 +0100

    refactor(post): make some modifications in post component
    add button component in posts component
    add padding bottom
    set height on style of each post
    item
    remove width and height attributes of img component

--- src/components/ui/Post/index.tsx
+++ src/components/ui/Post/index.tsx
@@ -3,23 +3,18 @@ import Link from 'next/link';
 import Img from '@/components/ui/Image';
 import Title from '@/components/ui/Typography/Title';
 import Text from '@/components/ui/Typography/Text';
+import { Button } from '../button';
 
 export default function Posts() {
   return (
-    <div className="mt-24 px-12 pt-24 space-y-16" data-testid="posts">
+    <div className="px-12 py-24 space-y-16" data-testid="posts">
       <div>
         <Title className="text-4xl">Our Posts</Title>
       </div>
       <ul className="flex space-x-4">
         {posts.map((post) => (
-          <li key={post.id} data-testid="post-item">
-            <Img
-              className="w-full h-full"
-              src={post.img}
-              alt=""
-              width={300}
-              height={100}
-            />
+          <li key={post.id} className="h-[12rem]" data-testid="post-item">
+            <Img className="w-full h-full" src={post.img} alt="" />
             <div className="mt-2 space-y-4">
               <span data-testid="date">{post.date}</span>
               <Link
@@ -30,6 +25,12 @@ export default function Posts() {
                 {post.title}
               </Link>
               <Text>{post.description}</Text>
+              <Button
+                className="capitalize hover:text-orange-600 bg-orange-600 text-white hover:bg-transparent hover:border-orange-600 hover:border delay-75"
+                variant={'link'}
+              >
+                read more
+              </Button>
             </div>
           </li>
         ))}
