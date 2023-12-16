4b690388f3ab0a524af5e26130db6e7617e6c7cf
teosoares10 <teosoares10@gmail.com>
2023-12-16 23:41:08 +0100

    refactor(ourpost): make changes in our post component
    remove posts constants, card component, ul element
    add our post list component

--- src/components/ui/OurPosts/index.tsx
+++ src/components/ui/OurPosts/index.tsx
@@ -1,40 +1,11 @@
-import { posts } from '@/constants/Local/posts';
 import Title from '../Typography/Title';
-import { Card } from '../Card';
+import OurPostList from './components/OurPostList';
 
 export default function OurPosts() {
   return (
     <div className="py-24 px-12 space-y-16" data-testid="our-posts">
       <Title className="text-4xl">Our Posts</Title>
-      <ul
-        className="flex justify-center space-x-4 flex-wrap"
-        data-testid="posts"
-      >
-        {posts
-          .filter((post) => {
-            return post.id < 4;
-          })
-          .map((post) => (
-            <li
-              key={post.id}
-              className="w-96 shadow hover:shadow-xl hover:bg-white/50 delay-150 pb-2"
-              data-testid="post-item"
-            >
-              <Card.Root className="">
-                <Card.Img src={post.img} className="w-full h-[12rem]" />
-                <div className="p-4 space-y-4">
-                  <Card.Date date={post.date} />
-                  <Card.Title
-                    title={post.title}
-                    className="text-left text-xl"
-                  />
-                  <Card.Content text={post.description} />
-                  <Card.Action link={`https://google.com`} text="read more" />
-                </div>
-              </Card.Root>
-            </li>
-          ))}
-      </ul>
+      <OurPostList />
     </div>
   );
 }
