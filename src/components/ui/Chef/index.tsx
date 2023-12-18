ef08a04d77fffe10a02f3e0f5524a59a90905c6e
teosoares10 <teosoares10@gmail.com>
2023-12-18 22:02:46 +0100

    refactor(chef): add card component

--- src/components/ui/Chef/index.tsx
+++ src/components/ui/Chef/index.tsx
@@ -1,7 +1,7 @@
-import Img from '@/components/ui/Image';
 import Title from '@/components/ui/Typography/Title';
 import { ChefApiResponse, ChefAttributes } from '@/@types/type';
 import SocialMedia from '@/components/ui/Icons/SocialMedia';
+import { Card } from '../Card';
 
 export default function Chef({ attributes }: ChefApiResponse) {
   return (
@@ -9,26 +9,24 @@ export default function Chef({ attributes }: ChefApiResponse) {
       <Title className="text-4xl">our masters chefs</Title>
       <div className="h-[20rem] flex gap-4">
         {attributes?.map((chef: ChefAttributes, index: number) => (
-          <div
+          <Card.Root
             key={index}
             className="w-[20rem] space-y-4"
             data-testid="chef-item"
           >
-            <Img
+            <Card.Img
               className="w-full h-full"
               src={`${chef.img}`}
               alt={`${chef.alt}`}
-              width={200}
-              height={250}
             />
-            <Title className="text-2xl text-left">{chef.name}</Title>
+            <Card.Title className="text-2xl text-left" title={chef.name} />
             <span className="block capitalize">{chef.role}</span>
             <SocialMedia
               facebook={chef.facebook}
               instagram={chef.instagram}
               twitter={chef.twitter}
             />
-          </div>
+          </Card.Root>
         ))}
       </div>
     </div>
