e9e2c4a837424775c881ccdbb6f65024eda255e8
teosoares10 <teosoares10@gmail.com>
2023-12-19 15:56:25 +0100

    refactor(cardcategory): add className

--- src/components/ui/Card/components/CardCategory.tsx
+++ src/components/ui/Card/components/CardCategory.tsx
@@ -2,8 +2,9 @@ import Text from '../../Typography/Text';
 
 type CardProps = {
   category: string;
+  className?: string;
 };
 
-export function CardCategory({ category }: CardProps) {
-  return <Text>{category}</Text>;
+export function CardCategory({ category, className }: CardProps) {
+  return <Text className={className}>{category}</Text>;
 }
