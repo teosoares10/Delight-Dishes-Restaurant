78697329989908dda46daff3864c9d5879aef207
teosoares10 <teosoares10@gmail.com>
2023-12-20 14:47:34 +0100

    refactor(footer.tsx): make some modifications
    update module paths
    rename all external components
    delete instagram component

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,27 +1,25 @@
-import ContactUs from './components/ContactUs';
-import OpenTime from './components/OpenTime';
-import Instagram from './components/SocialNetwork';
-import About from './components/About';
-import Menu from './components/Menu';
-import Copyright from './components/Copyright';
-import Newsletter from './components/Newsletter';
+import FooterContactUs from './FooterContactUs';
+import FooterOpenTime from './FooterOpenTime';
+import FooterAbout from './FooterAbout';
+import FooterMenu from './FooterMenu';
+import FooterCopyright from './FooterCopyright';
+import FooterNewsletter from './FooterNewsletter';
 
 export default function Footer() {
   return (
     <footer className="mt-24 pt-24 space-y-8" data-testid="footer">
-      <Newsletter />
+      <FooterNewsletter />
 
       <div className="flex justify-between px-12">
-        <About />
-        <Menu />
-        <Instagram />
+        <FooterAbout />
+        <FooterMenu />
         <div className="space-y-8">
-          <ContactUs />
-          <OpenTime />
+          <FooterContactUs />
+          <FooterOpenTime />
         </div>
       </div>
 
-      <Copyright />
+      <FooterCopyright />
     </footer>
   );
 }
