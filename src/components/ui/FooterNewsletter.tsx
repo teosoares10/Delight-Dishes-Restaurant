c608e07053eae10a41959d50f0242f8b447c2673
teosoares10 <teosoares10@gmail.com>
2023-12-21 17:39:19 +0100

    refactor(footernewsletter.tsx): update title component module path

--- src/components/ui/FooterNewsletter.tsx
+++ src/components/ui/FooterNewsletter.tsx
@@ -1,4 +1,4 @@
-import Title from '@/components/ui/Typography/Title';
+import Title from '@/components/ui/Title';
 import { Button } from '@/components/ui/button';
 
 export default function Newsletter() {
