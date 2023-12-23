c3c2a953aef5c2c671b29df22764c5afdb848f57
teosoares10 <teosoares10@gmail.com>
2023-12-23 13:34:18 +0100

    feat(booktable.tsx): add book table form component

--- src/components/ui/BookTable.tsx
+++ src/components/ui/BookTable.tsx
@@ -1,9 +1,11 @@
+import BookTableForm from './BookTableForm';
 import Title from './Title';
 
 export default function BookTable() {
   return (
     <div className="flex flex-col gap-8 justify-center items-center bg-gray-300 mt-32 py-32">
       <Title className="text-3xl">book a table</Title>
+      <BookTableForm />
     </div>
   );
 }
