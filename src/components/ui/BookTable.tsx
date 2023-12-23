b65f3b4c01c32d718eeed4072c593b2abfd396c9
teosoares10 <teosoares10@gmail.com>
2023-12-23 13:30:35 +0100

    refactor(booktable.tsx): remove form component

--- src/components/ui/BookTable.tsx
+++ src/components/ui/BookTable.tsx
@@ -1,11 +1,9 @@
-import Form from './Form';
 import Title from './Title';
 
 export default function BookTable() {
   return (
     <div className="flex flex-col gap-8 justify-center items-center bg-gray-300 mt-32 py-32">
       <Title className="text-3xl">book a table</Title>
-      <Form />
     </div>
   );
 }
