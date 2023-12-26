bd7b19a67611ae398b58d0ec53c6b0f3c9186aae
teosoares10 <teosoares10@gmail.com>
2023-12-26 01:16:59 +0100

    refactor(card): update module paths

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,11 +1,11 @@
-import { CardAction } from './CardAction';
-import { CardCategory } from './CardCategory';
-import { CardContent } from './CardContent';
-import { CardDate } from './CardDate';
-import { CardDescription } from './CardDescription';
-import { CardImg } from './CardImg';
-import { CardRoot } from './CardRoot';
-import { CardTitle } from './CardTitle';
+import { CardAction } from '@/components/ui/card-action';
+import { CardCategory } from '@/components/ui/card-category';
+import { CardContent } from '@/components/ui/card-content';
+import { CardDate } from '@/components/ui/card-date';
+import { CardDescription } from '@/components/ui/card-description';
+import { CardImg } from '@/components/ui/card-image';
+import { CardRoot } from '@/components/ui/card-root';
+import { CardTitle } from '@/components/ui/card-title';
 
 export const Card = {
   Root: CardRoot,
