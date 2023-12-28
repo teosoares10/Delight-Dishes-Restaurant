625ed299ff46f268ec5f99c2603daf13fefc6e04
teosoares10 <teosoares10@gmail.com>
2023-12-28 23:54:24 +0100

    refactor(nav): remove all data-testid attributes

--- src/components/ui/nav.tsx
+++ src/components/ui/nav.tsx
@@ -8,61 +8,37 @@ export default function NavBar({ className }: { className?: string }) {
     <>
       <ul className={cn('flex gap-4', className)}>
         <li>
-          <Link className="hover:text-orange-600" href="/" data-testid="link">
+          <Link className="hover:text-orange-600" href="/">
             Home
           </Link>
         </li>
         <li>
-          <Link
-            className="hover:text-orange-600"
-            href="/aboutUs"
-            data-testid="link"
-          >
+          <Link className="hover:text-orange-600" href="/aboutUs">
             About Us
           </Link>
         </li>
         <li>
-          <Link
-            className="hover:text-orange-600"
-            href="/ourMenu"
-            data-testid="link"
-          >
+          <Link className="hover:text-orange-600" href="/ourMenu">
             Our Menu
           </Link>
         </li>
         <li>
-          <Link
-            className="hover:text-orange-600"
-            href="/gallery"
-            data-testid="link"
-          >
+          <Link className="hover:text-orange-600" href="/gallery">
             Gallery
           </Link>
         </li>
         <li>
-          <Link
-            className="hover:text-orange-600"
-            href="/recipe"
-            data-testid="link"
-          >
+          <Link className="hover:text-orange-600" href="/recipe">
             Recipe
           </Link>
         </li>
         <li>
-          <Link
-            className="hover:text-orange-600"
-            href="/blog"
-            data-testid="link"
-          >
+          <Link className="hover:text-orange-600" href="/blog">
             Blog
           </Link>
         </li>
         <li>
-          <Link
-            className="hover:text-orange-600"
-            href="/contact"
-            data-testid="link"
-          >
+          <Link className="hover:text-orange-600" href="/contact">
             Contact
           </Link>
         </li>
