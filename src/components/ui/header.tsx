42a99eea177389ad67d0177d7e4e7387b22d06a7
teosoares10 <teosoares10@gmail.com>
2023-12-29 00:30:08 +0100

    refactor(header): move book table link into a wrapper

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,24 +1,27 @@
 import Logo from '@/components/ui/logo';
 import NavBar from '@/components/ui/nav';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <header className="flex justify-between items-center p-4">
       <Logo />
       <NavBar />
-      <Button
-        className="text-white hover:text-orange-500 border border-orange-500 bg-orange-500"
-        asChild
-      >
-        <a
-          href="#bookTable"
-          className="block capitalize"
-          data-testid="book-table-link"
+      <div className="flex">
+        <Button
+          className="text-white hover:text-orange-500 border border-orange-500 bg-orange-500"
+          asChild
         >
-          book a table
-        </a>
-      </Button>
+          <a
+            href="#bookTable"
+            className="block capitalize"
+            data-testid="book-table-link"
+          >
+            book a table
+          </a>
+        </Button>
+      </div>
     </header>
   );
 }
