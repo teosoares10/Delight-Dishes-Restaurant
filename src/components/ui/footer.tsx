02824558e1800191041276bec71cd3ed6e27035e
teosoares10 <teosoares10@gmail.com>
2023-12-31 00:08:58 +0100

    refactor(footer): update module path and rename newsletter element

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -3,12 +3,11 @@ import FooterOpenTime from '@/components/ui/footer-open-time';
 import FooterAbout from '@/components/ui/footer-about';
 import FooterMenu from '@/components/ui/footer-menu';
 import FooterCopyright from '@/components/ui/footer-copyright';
-import FooterNewsletter from '@/components/ui/footer-newsletter';
+import Newsletter from '@/components/ui/newsletter';
 
 export default function Footer() {
   return (
     <footer className="pt-24 space-y-8">
-      <FooterNewsletter />
       <div className="flex justify-between px-12">
         <FooterAbout />
         <FooterMenu />
@@ -16,6 +15,7 @@ export default function Footer() {
           <FooterContactUs />
           <FooterOpenTime />
         </div>
+        <Newsletter />
       </div>
 
       <FooterCopyright />
