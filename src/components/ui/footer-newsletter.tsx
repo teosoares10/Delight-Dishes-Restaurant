7e34083bafe72158cf86ca0cf258d021f9db1ff0
teosoares10 <teosoares10@gmail.com>
2023-12-31 00:05:10 +0100

    style(footer-newsletter): change almost styles

--- src/components/ui/footer-newsletter.tsx
+++ src/components/ui/footer-newsletter.tsx
@@ -3,24 +3,23 @@ import { Button } from '@/components/ui/button';
 
 export default function Newsletter() {
   return (
-    <div className="flex h-[10rem] bg-gray-400">
-      <div className="w-[30rem] bg-gray-500 pl-12 pt-14">
+    <div className="w-[20rem] space-y-4">
+      <div>
         <span className="uppercase" data-testid="span">
           subscribe to our
         </span>
         <Title className="text-2xl text-left uppercase">newsletter</Title>
       </div>
-      <div className="flex-1 pl-48 pt-14">
-        <div className="flex items-center gap-1 bg-white p-2 w-[40rem] rounded-full">
-          <input
-            className="w-full h-9 text-black pl-2 rounded-full"
-            type="text"
-            placeholder="Enter email address"
-          />
-          <Button className="w-[10rem] bg-orange-600 hover:bg-orange-400 rounded-full uppercase">
-            submit
-          </Button>
-        </div>
+
+      <div className="flex flex-col space-y-2">
+        <input
+          className="border border-orange-600 text-black p-2 rounded-lg"
+          type="text"
+          placeholder="Enter email address"
+        />
+        <Button className="w-24 ml-auto bg-orange-600 hover:bg-orange-400 text-white rounded-lg uppercase">
+          submit
+        </Button>
       </div>
     </div>
   );
