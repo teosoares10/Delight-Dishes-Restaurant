5a379fb6cc732fa0fb7857341f095e994fdbff80
teosoares10 <teosoares10@gmail.com>
2024-01-01 22:16:56 +0100

    feat: add tanstack query

---
+++ frontend/src/components/providers/tanstack-provider.tsx
@@ -0,0 +1,27 @@
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
+
+export const TanstackProvider = ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+            refetchOnWindowFocus: false
+          }
+        }
+      })
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
