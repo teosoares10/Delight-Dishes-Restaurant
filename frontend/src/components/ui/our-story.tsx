b434dfd0a83d0ceed8e7c21673bf02c4e460ce02
teosoares10 <teosoares10@gmail.com>
2024-01-03 17:35:55 +0100

    refactor(our-story): make some modification  - put in a wrapper imgs, title, text and button and set a flex in its style  - add a new main title for the our story

--- frontend/src/components/ui/our-story.tsx
+++ frontend/src/components/ui/our-story.tsx
@@ -7,37 +7,40 @@ import { Button } from '@/components/ui/button';
 export default function OurStory() {
   return (
     <div className="flex justify-between items-center py-36 px-12">
-      <Img
-        src="https://images.unsplash.com/photo-1583394293214-28ded15ee548?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80"
-        className="w-[20rem] h-[30rem]"
-        width={350}
-        height={450}
-        alt=""
-      />
-      <div className="space-y-8">
-        <Title className="text-4xl text-left">our story</Title>
-        <Text className="w-[35rem]">
-          Born from a passion for culinary excellence, our story began with a
-          dream to redefine dining. Every dish tells a tale of creativity and
-          craftsmanship, inviting you on a journey through flavor. From humble
-          beginnings to a gastronomic haven, our story is a celebration of the
-          extraordinary in every bite. Join us in creating memories, one
-          extraordinary meal at a time.
-        </Text>
-        <Button className="capitalize bg-orange-600" asChild>
-          <Link href={``} data-testid="link">
-            learn more
-          </Link>
-        </Button>
-      </div>
+      <Title className="text-4xl text-left">our story</Title>
+      <div className="flex">
+        <Img
+          src="https://images.unsplash.com/photo-1583394293214-28ded15ee548?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80"
+          className="w-[20rem] h-[30rem]"
+          width={350}
+          height={450}
+          alt=""
+        />
+        <div className="space-y-8">
+          <Title className="text-4xl text-left">our story</Title>
+          <Text className="w-[35rem]">
+            Born from a passion for culinary excellence, our story began with a
+            dream to redefine dining. Every dish tells a tale of creativity and
+            craftsmanship, inviting you on a journey through flavor. From humble
+            beginnings to a gastronomic haven, our story is a celebration of the
+            extraordinary in every bite. Join us in creating memories, one
+            extraordinary meal at a time.
+          </Text>
+          <Button className="capitalize bg-orange-600" asChild>
+            <Link href={``} data-testid="link">
+              learn more
+            </Link>
+          </Button>
+        </div>
 
-      <Img
-        src="https://images.unsplash.com/photo-1583394293214-28ded15ee548?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80"
-        className="w-[20rem] h-[30rem]"
-        width={350}
-        height={450}
-        alt=""
-      />
+        <Img
+          src="https://images.unsplash.com/photo-1583394293214-28ded15ee548?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80"
+          className="w-[20rem] h-[30rem]"
+          width={350}
+          height={450}
+          alt=""
+        />
+      </div>
     </div>
   );
 }
