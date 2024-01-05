2683fc208dbea1ef222bdd395c1739216fe3099e
teosoares10 <teosoares10@gmail.com>
2024-01-05 14:46:02 +0100

    refactor(contact-info): remove title element

--- frontend/src/components/ui/contact-info.tsx
+++ frontend/src/components/ui/contact-info.tsx
@@ -1,4 +1,3 @@
-import Title from '@/components/ui/title';
 import { EnvelopeSimple, MapPin, Phone } from '@phosphor-icons/react';
 import FooterContactWithIcon from '@/components/ui/footer-contact-with-icon';
 import { cn } from '@/lib/utils';
@@ -10,7 +9,6 @@ type ContactInfoProps = {
 export default function ContactInfo({ className }: ContactInfoProps) {
   return (
     <div className={cn('space-y-4', className)}>
-      <Title className="text-xl text-left">contact us</Title>
       <div className="space-y-4">
         <FooterContactWithIcon icon={<MapPin size={20} />} contact="address" />
 
