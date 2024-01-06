50921ceef2328bd4a6f5018ca7e970401420a46d
teosoares10 <teosoares10@gmail.com>
2024-01-06 18:04:27 +0100

    refactor(service): remove service list element and add a list of service item elements

--- frontend/src/components/ui/service.tsx
+++ frontend/src/components/ui/service.tsx
@@ -1,9 +1,20 @@
-import ServiceList from '@/components/ui/service-list';
+import { services } from 'src/constants/Local/services';
+import ServiceIcon from '@/components/ui/service-icon';
+import ServiceItem from '@/components/ui/service-item';
 
 export default function Service() {
   return (
     <div className="border-spacing-4 space-y-16 text-center mb-36 px-12">
-      <ServiceList />
+      <div className="flex justify-between">
+        {services.map((service) => (
+          <ServiceItem
+            key={service.id}
+            icon={<ServiceIcon icon={service.type} />}
+            title={service.title}
+            description={service.description}
+          />
+        ))}
+      </div>
     </div>
   );
 }
