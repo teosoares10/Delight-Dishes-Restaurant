5dcf7e466b24940d833cb5c8e922bca02d43b9e5
teosoares10 <teosoares10@gmail.com>
2024-01-06 17:58:12 +0100

    refactor(service): remove title element

--- frontend/src/components/ui/service.tsx
+++ frontend/src/components/ui/service.tsx
@@ -1,10 +1,8 @@
-import Title from '@/components/ui/title';
 import ServiceList from '@/components/ui/service-list';
 
 export default function Service() {
   return (
     <div className="border-spacing-4 space-y-16 text-center mb-36 px-12">
-      <Title className="text-4xl">catering service</Title>
       <ServiceList />
     </div>
   );
