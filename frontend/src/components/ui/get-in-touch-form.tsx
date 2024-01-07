7d5ff53e1e2b4a3d066144fab923c45dfe8fd85d
teosoares10 <teosoares10@gmail.com>
2024-01-07 12:47:34 +0100

    refactor(get-in-touch-form): make some modifications  - create get in touch form props type  - add react hook form  - add zod  - add zod resolver  - add get in touch form schema validation  - add wai-aria  - add input error component

--- frontend/src/components/ui/get-in-touch-form.tsx
+++ frontend/src/components/ui/get-in-touch-form.tsx
@@ -1,24 +1,59 @@
+import { z } from 'zod';
 import { InputWithLabel } from './input-with-label';
 import { TextareaInputWithLabel } from './textarea-input-with-label';
+import getInTouchformSchema from '@/validation/get-in-touch-form-schema';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import InputError from './input-error';
+
+type GetInTouchFormProps = z.infer<typeof getInTouchformSchema>;
 
 export function GetInTouchForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<GetInTouchFormProps>({
+    resolver: zodResolver(getInTouchformSchema)
+  });
+
+  const handleOnSubmit: SubmitHandler<GetInTouchFormProps> = (data) =>
+    console.log(data);
+
   return (
-    <form action="" className="space-y-4">
+    <form
+      action=""
+      onSubmit={handleSubmit(handleOnSubmit)}
+      className="space-y-4"
+    >
       <InputWithLabel
         label="Name"
         placeholder="Enter your name"
         className="space-y-1"
+        aria-describedby="name-errors"
+        {...register('name')}
       />
+      {errors.name ? (
+        <InputError id="name-errors" error={`${errors.name.message}`} />
+      ) : null}
+
       <InputWithLabel
         type="email"
         label="E-mail"
         placeholder="Enter your name"
         className="space-y-1"
+        aria-describedby="email-errors"
+        {...register('name')}
       />
+      {errors.email ? (
+        <InputError id="email-errors" error={`${errors.email.message}`} />
+      ) : null}
+
       <TextareaInputWithLabel
         label="Message"
         placeholder="Enter your message"
         className="flex flex-col space-y-1"
+        {...register('message')}
       />
       <button
         type="submit"
