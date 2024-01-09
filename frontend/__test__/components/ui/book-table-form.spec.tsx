8bfc52069427d5c1fab65ad206a723ce5c07884c
teosoares10 <teosoares10@gmail.com>
2024-01-09 14:29:03 +0100

    test(book-table-form): add a ref and assertions to should render default correctly

--- frontend/__test__/components/ui/book-table-form.spec.tsx
+++ frontend/__test__/components/ui/book-table-form.spec.tsx
@@ -3,9 +3,15 @@ import { render, screen } from '@testing-library/react';
 import BookTableForm from '@/components/ui/book-table-form';
 
 describe('<BookTableForm>', () => {
+  const ref = React.createRef<HTMLFormElement>();
+  render(<BookTableForm ref={ref} />);
+
   it('should render default correctly', () => {
-    const ref = React.createRef<HTMLFormElement>();
-    render(<BookTableForm ref={ref} />);
+    expect(screen.getByLabelText('nº of guest (s)')).toBeInTheDocument();
+    expect(screen.getByRole('combobox')).toBeInTheDocument();
+    expect(screen.getAllByRole('option')).toHaveLength(3);
+    expect(screen.getByPlaceholderText('Enter your time')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Enter your date')).toBeInTheDocument();
     expect(screen.getByRole('button')).toBeInTheDocument();
   });
 });
