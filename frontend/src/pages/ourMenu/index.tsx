8103d2ef530ea8a5f414bfdbe7dee3872ce448d4
teosoares10 <teosoares10@gmail.com>
2024-01-10 23:41:39 +0100

    styles(our-menu): add font size and text transform to first text in header

--- frontend/src/pages/ourMenu/index.tsx
+++ frontend/src/pages/ourMenu/index.tsx
@@ -10,7 +10,7 @@ export default function OurMenu() {
     <div>
       <header className="flex justify-between border-b border-black">
         <div>
-          <Text>
+          <Text className="text-2xl uppercase">
             best <span className="block">in town</span>
           </Text>
         </div>
