0dc9c2c96aaca9d21f8081a4f5f635d0eb4f03e3
teosoares10 <teosoares10@gmail.com>
2024-01-11 17:46:25 +0100

    styles(book-table): set new padding to top and bottom

--- frontend/src/components/ui/book-table.tsx
+++ frontend/src/components/ui/book-table.tsx
@@ -3,7 +3,7 @@ import Title from '@/components/ui/title';
 
 export default function BookTable() {
   return (
-    <div className="flex flex-col gap-8 justify-center items-center bg-gray-300 mt-32 py-32">
+    <div className="flex flex-col gap-8 justify-center items-center bg-gray-300 mt-32 py-4">
       <Title className="text-3xl">book a table</Title>
       <BookTableForm />
     </div>
