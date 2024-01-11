2a6f5a4036bd45ca38e3b9ce11a09a730bcb6a23
teosoares10 <teosoares10@gmail.com>
2024-01-11 11:23:40 +0100

    test(book-table-form): create a new test case and move render to all test case

--- frontend/__test__/components/ui/book-table-form.spec.tsx
+++ frontend/__test__/components/ui/book-table-form.spec.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import BookTableForm from '@/components/ui/book-table-form';
 
 describe('<BookTableForm>', () => {
   const ref = React.createRef<HTMLFormElement>();
-  render(<BookTableForm ref={ref} />);
 
   it('should render default correctly', () => {
+    render(<BookTableForm ref={ref} />);
     expect(screen.getByLabelText('nº of guest (s)')).toBeInTheDocument();
     expect(screen.getByRole('combobox')).toBeInTheDocument();
     expect(screen.getAllByRole('option')).toHaveLength(3);
@@ -14,4 +15,19 @@ describe('<BookTableForm>', () => {
     expect(screen.getByPlaceholderText('Enter your date')).toBeInTheDocument();
     expect(screen.getByRole('button')).toBeInTheDocument();
   });
+
+  it('should allow user select option', async () => {
+    render(<BookTableForm ref={ref} />);
+    await userEvent.selectOptions(screen.getByRole('combobox'), '3 Person');
+    expect(
+      (
+        screen.getByRole('option', {
+          name: '3 Person',
+          hidden: true
+        }) as HTMLOptionElement
+      ).selected
+    ).toBe(true);
+  });
+
+  screen.debug;
 });
