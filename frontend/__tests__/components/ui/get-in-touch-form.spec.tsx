0aae38a4e9734dbe448ae032b5e3204bdcd6632a
teosoares10 <teosoares10@gmail.com>
2024-01-14 17:15:12 +0100

    refactor(get-in-touch-form): create submit form describe and add submit when inputs are empty test case

--- frontend/__tests__/components/ui/get-in-touch-form.spec.tsx
+++ frontend/__tests__/components/ui/get-in-touch-form.spec.tsx
@@ -1,5 +1,6 @@
 import GetInTouchForm from '@/components/ui/get-in-touch-form';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 
 describe('<GetInTouchForm />', () => {
@@ -10,5 +11,16 @@ describe('<GetInTouchForm />', () => {
 
     expect(screen.getAllByRole('textbox')).toHaveLength(3);
     expect(screen.getByRole('button')).toBeVisible();
+  });
+
+  describe('tests for submit form', () => {
+    it('should not submit when inputs are empty', async () => {
+      render(<GetInTouchForm ref={ref} />);
+      expect(screen.getByPlaceholderText('Enter your name')).toHaveValue('');
+      expect(screen.getByPlaceholderText('Enter your email')).toHaveValue('');
+      expect(screen.getByPlaceholderText('Enter your message')).toHaveValue('');
+      await userEvent.click(screen.getByRole('button'));
+      expect(screen.getAllByRole('Alert')).toHaveLength(2);
+    });
   });
 });
