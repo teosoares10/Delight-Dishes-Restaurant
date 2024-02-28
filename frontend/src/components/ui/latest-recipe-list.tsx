1e472a652cb86989f6d0a474bb619c3c5ad78f3f
Teodoro Pedro <teosoares10@gmail.com>
2024-02-28 18:02:36 +0100

    refactor(latest-recipe-list): add strapi recipe api data
    receive data and show the latest recipes

--- frontend/src/components/ui/latest-recipe-list.tsx
+++ frontend/src/components/ui/latest-recipe-list.tsx
@@ -1,24 +1,39 @@
 import { Timer } from '@phosphor-icons/react';
 import { Card } from './card';
 import Link from 'next/link';
+import { RecipeData } from '@/@types/recipe';
 
-export function LatestRecipeList() {
+export function LatestRecipeList({ data }: RecipeData) {
+  const recipesLength = data.length;
   return (
     <ul className="flex justify-between items-center">
       <li>
-        <Link href="">
+        <Link
+          href="/recipe/[slug]"
+          as={`recipe/${data[recipesLength - 2].attributes.slug}`}
+        >
           <Card.Root className="space-y-4 group">
-            <Card.Img src="" className="w-[19rem] h-[14rem]" />
+            <Card.Img
+              src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${data[
+                recipesLength - 2
+              ].attributes.image.data?.attributes?.formats.medium.url}`}
+              className="w-[19rem] h-[14rem]"
+            />
             <div className="px-4 pb-4 space-y-4">
               <Card.Title
-                title="air fryer steak fries"
+                title={data[recipesLength - 2].attributes.title}
                 className="text-left text-xl group-hover:underline"
               />
               <div className="flex justify-between">
-                <Card.Category category="sides" className="" />
+                <Card.Category
+                  category={data[recipesLength - 2].attributes.category}
+                  className=""
+                />
                 <div className="flex items-center gap-1">
                   <Timer size={20} />
-                  <Card.Description description="30 minutes" />
+                  <Card.Description
+                    description={data[recipesLength - 2].attributes.time_total}
+                  />
                 </div>
               </div>
             </div>
