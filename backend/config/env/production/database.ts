82356f1b84832ca60df0c039d2017310bec6e2ae
Teodoro Pedro <teosoares10@gmail.com>
2024-02-29 00:13:13 +0100

    chore: add postgres and its database config

--- backend/config/env/production/database.ts
+++ backend/config/env/production/database.ts
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
             password: env('DATABASE_PASSWORD', 'strapi'),
             schema: env('DATABASE_SCHEMA', 'public'),
             ssl: {
-                rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false) // For self-signed certificates
+                ca: env('DATABASE_CA')
             },
         },
         debug: false,
