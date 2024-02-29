7089b824985630436deaadd909d5c792c1fb43db
Teodoro Pedro <teosoares10@gmail.com>
2024-02-29 17:26:02 +0100

    refator(card-title): create a new prop
    create asChild prop which allow the parent be like the children and children with type of react node to accept element like h2

--- frontend/src/components/ui/card-title.tsx
+++ frontend/src/components/ui/card-title.tsx
@@ -1,10 +1,16 @@
 import Title from '@/components/ui/title';
 
 type CardProps = {
-  title: string;
+  title?: string;
+  children?: React.ReactNode;
   className?: string;
+  asChild?: boolean;
 };
 
-export function CardTitle({ title, className }: CardProps) {
-  return <Title className={className}>{title}</Title>;
+export function CardTitle({ children, title, className, asChild }: CardProps) {
+  return (
+    <Title className={className} asChild={asChild}>
+      {title ? title : children}
+    </Title>
+  );
 }
