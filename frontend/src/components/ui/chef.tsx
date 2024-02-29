f6f85e1d59e0b44476676fadcadb14ccd24e179e
Teodoro Pedro <teosoares10@gmail.com>
2024-02-29 17:37:33 +0100

    refactor(chef): make a few changes
    - remove chef-list component and its type.
    - create chef prop type
    - set asChild to title component

--- frontend/src/components/ui/chef.tsx
+++ frontend/src/components/ui/chef.tsx
@@ -1,14 +1,16 @@
 import Title from '@/components/ui/title';
-import { ChefApiResponse } from '@/@types/type';
-import { ChefList } from './chef-list';
 
-export default function Chef({ attributes }: ChefApiResponse) {
+type ChefProps = {
+  chefList: JSX.Element;
+};
+
+export default function Chef({ chefList }: ChefProps) {
   return (
     <div className="space-y-16 py-36 px-12">
-      <Title className="text-4xl">our masters chefs</Title>
-      <div className="h-[20rem] flex gap-4">
-        <ChefList chefs={attributes} />
-      </div>
+      <Title className="text-4xl" asChild>
+        <h2>our masters chefs</h2>
+      </Title>
+      <div className="h-[20rem] flex gap-4">{chefList}</div>
     </div>
   );
 }
